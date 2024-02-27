ceb3f9c5e7fd9350b1d9130272418a12bb8598c2
Safar Yasin Shajahan <116191536+Safarys@users.noreply.github.com>
2024-02-27 15:06:25 +0530

    Add files via upload

---
+++ oops/fip.java
@@ -0,0 +1,19 @@
+class fip{
+public static void main(String a[])
+{
+System.out.println("You are MACE");
+System.out.println("Roll no:"+a[0]);
+int r=Integer.parseInt(a[0]);
+System.out.println("Mark 1:"+a[1]);
+int m1=Integer.parseInt(a[1]);
+System.out.println("Mark 2:"+a[2]);
+int m2=Integer.parseInt(a[2]);
+int t=m1+m2;
+System.out.println("Total Marks:"+t);
+show();
+}
+static void show()
+{
+System.out.println("Welcome");
+}
+}
